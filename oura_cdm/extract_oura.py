7aa50dcab16c3717322841734cdd1d5280943edc
Jeremy Mann <jmann277@gmail.com>
2022-03-02 17:39:25 -0800

    allow for invalid tokens

--- oura_cdm/extract_oura.py
+++ oura_cdm/extract_oura.py
@@ -46,6 +46,10 @@ def get_oura_data(
         start_date = '2000-02-01'
     log_info_e(f'Getting data from oura from {start_date} to {end_date}')
     url = f'https://api.ouraring.com/v1/sleep?start={start_date}&end={end_date}'
-    ans = requests.get(url, params={"access_token": token}).json()
+    ans = requests.get(url, params={"access_token": token})
+    if ans.status_code == 401:
+        log_warning_e('No token provided running pipeline on mock data')
+        return _get_mock_oura_data()[:0]
+    ans = ans.json()
     log_info_e('Oura query successful')
     return ans['sleep']
