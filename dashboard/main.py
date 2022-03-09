a6cf2815f37bebcf5fa72d2f37dc1dc63c6d585a
Jeremy Mann <jmann277@gmail.com>
2022-03-08 20:02:25 -0800

    refactor dashboard to not use journey_df

--- dashboard/main.py
+++ dashboard/main.py
@@ -17,65 +17,67 @@ def make_dashboard():
     artifacts = get_artifacts()
     ontology = Ontology()
     observation_df = artifacts[Artifact.OBSERVATION]
-    journey_df: pd.DataFrame = artifacts[Artifact.JOURNEY].copy()
     source_data: pd.DataFrame = artifacts[Artifact.SOURCE_DATA]
 
     renaming = {c: ontology.get_concept_name_from_id(
-        c) for c in journey_df.columns}
-    journey_df = journey_df.rename(columns=renaming)
-    melted_journey_df = pd.melt(
-        journey_df, value_vars=list(journey_df.columns), ignore_index=False)
-    duration_column = 'Duration (hours)'
-
-    melted_journey_df['minutes'] = (
-        (melted_journey_df['value']
+        c) for c in observation_df.observation_concept_id.unique()}
+    visualization_df = observation_df.copy()
+    unit = ontology.get_concept_name(
+        ontology.get_concept_from_id(
+            observation_df['unit_concept_id'].iloc[0]
+        )
+    )
+    visualization_df["observation_concept_name"] = \
+        observation_df.observation_concept_id.replace(renaming)
+    renaming = {c: ontology.get_concept_name_from_id(
+        c) for c in observation_df.observation_concept_id.unique()}
+    visualization_df = visualization_df[
+        ['observation_date', 'value_as_number', 'observation_concept_name']]
+    visualization_df['observation_date'] = pd.to_datetime(
+        visualization_df['observation_date'])
+    pd_unit = unit[0].upper()
+    time_value = pd.to_timedelta(
+        visualization_df['value_as_number'], unit=pd_unit)
+    visualization_df['value'] = time_value / pd.Timedelta('1 hour')
+    visualization_df['minutes'] = (
+        (time_value
          / pd.Timedelta('1 minute')).astype(int)
         % 60
     ).astype(str)
-    melted_journey_df['hours'] = (
-        (melted_journey_df['value']
+    visualization_df['hours'] = (
+        (time_value
          / pd.Timedelta('1 hour')).astype(int)
     ).astype(str)
-
-    melted_journey_df[duration_column] = melted_journey_df.value \
-        / pd.Timedelta('1 hour')
-    melted_journey_df['Duration (hours)'] = np.round(
-        melted_journey_df['Duration (hours)'], 2)
-    melted_journey_df = melted_journey_df.drop(columns=['value', ])
-    melted_journey_df = melted_journey_df.reset_index()
-    melted_journey_df['description'] = (
-        + melted_journey_df['hours'] + ':'
-        + melted_journey_df['minutes']
+    visualization_df['description'] = (
+        + visualization_df['hours'] + ':'
+        + visualization_df['minutes']
         + ' '
-        + melted_journey_df['observation_date'].astype(str)
+        + visualization_df['observation_date'].astype(str)
         + ' '
-        + melted_journey_df['observation_concept_id'].astype(str)
+        + visualization_df['observation_concept_name'].astype(str)
     )
 
-    st.title("Observation Graph")
-
-    width = min(len(melted_journey_df) * 10, 500)
+    width = min(len(visualization_df) * 10, 800)
     single_nearest = alt.selection_single(on='mouseover', nearest=True)
-    chart = alt.Chart(melted_journey_df).mark_point().encode(
+    chart = alt.Chart(visualization_df).mark_point().encode(
         x='observation_date',
-        y=duration_column,
-        color='observation_concept_id',
+        y='value',
+        color='observation_concept_name',
         tooltip='description'
     ).add_selection(single_nearest).interactive().properties(
         width=width,
     )
-
     st.altair_chart(chart)
-
-    st.title("Observation Table")
-    st.table(observation_df.head(5))
     st.title("Source Data")
     st.write(source_data[0])
+    st.title("Observation Table")
+    st.table(observation_df.head(5))
     st.title("Ontology Tables")
     st.title("Concepts")
     st.table(artifacts[Artifact.CONCEPT].reset_index())
     st.title("Concept Relationships")
     st.table(artifacts[Artifact.CONCEPT_RELATIONSHIP].reset_index())
+
 
 if __name__ == '__main__':
     make_dashboard()
